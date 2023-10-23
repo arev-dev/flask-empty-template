38eeda74e020422f46a4fe2d79da397f022371a0
Daniel Rodriguez <arevdevgames@gmail.com>
2023-10-23 05:01:10 -0600

    deploy process and index added(testing)

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from decouple import config
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "ALSKLAKSALSKAKSLASKLAKSKLAS"
+
+
+app.run(host="0.0.0.0", port=config("PORT"))
 
 
