cbf6b5fa3d03f612406e31638890afcc9991e03d
Daniel Rodriguez <arevdevgames@gmail.com>
2023-10-23 04:53:09 -0600

    deploy process and index added(testing)

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from decouple import config
+
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "ALSKLAKSALSKAKSLASKLAKSKLAS"
+
 if __name__ == '__main__':
     app.run()
+    
